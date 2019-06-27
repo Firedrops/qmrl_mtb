7cc49ca361f22e60e62a07d10e685757bbbe8719
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-06-27 16:06:53 +1000

    placeholder pass txts

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 export DockerUsername="foo" #replace foo in quotes with your username, retain the quotation marks.
 export lpass="~/qimr_mtb/lpass.txt" #path to .txt containing linux password
+#By default, sudo password is only prompted once every 15 minutes, so I have chosen to pass it only once per if loop to simplify commands.
 export dpass="~/qimr_mtb/dpass.txt" #path to .txt containing docker password
 
 #Checks if Docker is installed
@@ -10,20 +13,20 @@ if [ "$dockercheck1 -ge 1" ]; then
 else
   echo "Docker not found, attempting installation."
   cat $lpass | sudo -S apt-get -y update
-  cat $lpass | sudo -S apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-  cat $lpass | sudo -S curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  cat $lpass | sudo -S add-apt-repository \
+  sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-  cat $lpass | sudo -S apt-get -y update
-  cat $lpass | sudo -S apt-get -y install docker-ce docker-ce-cli containerd.io
+  sudo apt-get -y update
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io
   echo "sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common"
   echo "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
   echo "sudo apt-get -y update"
   echo "sudo apt-get -y install docker-ce docker-ce-cli containerd.io"
   #Checks for successful installation
-  dockercheck2=$(docker run hello world | grep "working correctly" | wc -l)
+  dockercheck2=$(sudo docker run hello-world | grep "working correctly" | wc -l)
   if [ "$dockercheck2 -ge 1" ]; then
     echo "Docker has been successfully installed, proceeding."
   else
