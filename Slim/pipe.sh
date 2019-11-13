faf48875f96f728143089e49ebf84e089676057e
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-11-13 17:29:39 +1100

    Update pipe.sh

--- Slim/pipe.sh
+++ Slim/pipe.sh
@@ -5,6 +5,7 @@ shift
 
 bwa index /data/reference.fasta
 samtools faidx /data/reference.fasta
+picard CreateSequenceDictionary R=/data/reference.fasta O=/data/reference.dict
 
 java -jar /trimmomatic/classes/trimmomatic.jar PE -phred33 -trimlog ${file}_log.txt ${file}_R1.fastq.gz ${file}_R2.fastq.gz ${file}_paired_R1.fastq.gz ${file}_unpaired_R1.fastq.gz ${file}_paired_R2.fastq.gz ${file}_unpaired_R2.fastq.gz ILLUMINACLIP:/trimmomatic/adapters/NexteraPE-PE.fa:2:30:10 LEADING:10 TRAILING:10 SLIDINGWINDOW:4:15 MINLEN:36
 
