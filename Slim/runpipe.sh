6345602465f9415e98fb42cf8e2415480e1801dd
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-11-15 18:24:26 +1100

    Update runpipe.sh

--- Slim/runpipe.sh
+++ Slim/runpipe.sh
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=64000 # mb
+#SBATCH --mem=16000 # mb
 #SBATCH --time=9999:00:00
 
 n=$SLURM_ARRAY_TASK_ID
 nme=$(head -n $n /home/larry/png_mtb/filesin.txt | tail -1)
 
-docker run -it --rm -v /Backup/Data/Projects/PNG_TB/:/data/ dockersubtest/qimr_mtb:slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
+docker run -it --rm -v /home/larry/png_mtb/:/data/ dockersubtest/qimr_mtb:slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
