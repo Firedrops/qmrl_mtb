365225743142c41a1443d6e6aa454910d338b070
Lachlan Coin <ljmcoin@gmail.com>
2019-11-15 21:01:02 +1000

    updated runpipe

--- Slim/runpipe.sh
+++ Slim/runpipe.sh
@@ -8,6 +8,7 @@
 #SBATCH --mem=32000MB
 
 n=$SLURM_ARRAY_TASK_ID
-nme=$(head -n $n filesin.txt | tail -1 | cut -f 1 -d '_')
-
+pwd=$(pwd)
+nme=$(ls ${pwd}/data  | grep _R1.fastq.gz | sort | head -n $n  | tail -1 | cut -f 1 -d '_')
+echo "running "$nme
 docker run -it --rm -v ${pwd}/:/data/ dockersubtest/qimr_mtb:slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
