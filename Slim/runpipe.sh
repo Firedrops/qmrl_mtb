a33cc8d68d2470849b056533e835c19dcd4f218d
Lachlan Coin <lachlan.coin@unimelb.edu.au>
2019-11-15 14:56:16 +1100

    slurm support

---
+++ Slim/runpipe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=TBpipe
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=64000 # mb
+#SBATCH --time=9999:00:00
+
+n=$SLURM_ARRAY_TASK_ID
+nme=$(head -n $n filesin.txt | tail -1)
+
+docker run -it --rm -v /Backup/Data/Projects/PNG_TB/:/data/ qimr_slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
+
+   
