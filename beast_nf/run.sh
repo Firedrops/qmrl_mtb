26ebc85fb23976c193370c9805d8e55ba363edf8
Lachlan Coin <ljmcoin@gmail.com>
2019-11-15 21:59:01 +1000

    update

--- beast_nf/run.sh
+++ beast_nf/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=Kallisto
+#SBATCH --job-name=beast
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=64000 # mb
+#SBATCH --mem=16000 # mb
 #SBATCH --time=9999:00:00
 
-JAVA_LIB="/swold/beast/BEASTv1.8.2/lib:/usr/local/lib:/swold/beast/beagle-lib/lib/"
+JAVA_LIB="/swold/beast/BEASTv1.8.2/lib:/usr/local/lib:/home/lcoin/lib"
 JAVA_CP="/swold/beast/BEASTv1.8.2/lib/beast.jar:/swold/beast/BEASTv1.8.2/lib/beast-beagle.jar"
+#JAVA_LIB="/swold/beast/BEASTv1.10.2/lib:/usr/local/lib:/home/lcoin/lib:"
+#JAVA_CP="/swold/beast/BEASTv1.10.2/lib/beast.jar:/swold/beast/BEASTv1.10.2/lib/beast-beagle.jar"
 
-java -Xms64m -Xmx8092m -Djava.library.path=$JAVA_LIB -cp $JAVA_CP dr.app.beast.BeastMain ./data/DARU_95_real_f5_beast1_trial1_3.xml 
+#java -Xms64m -Xmx8092m -Djava.library.path=/swold/beast/BEASTv1.10.2/lib:/usr/local/lib:/home/lcoin/lib: -cp /swold/beast/BEASTv2.10.2/lib/beast.jar:/swold/beast/BEASTv1.10.2/lib/beast-beagle.jar dr.app.beast.BeastMain DARU_95_real_f5_beast1_trial1_3.xml
+java -Xms64m -Xmx8092m -Djava.library.path=$JAVA_LIB -cp $JAVA_CP dr.app.beast.BeastMain DARU_95_real_f5_beast1_trial1_3.xml 
