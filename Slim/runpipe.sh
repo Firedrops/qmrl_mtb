e02a8daba36733591b34ea50dbf41780ebf92eb0
Lachlan Coin <ljmcoin@gmail.com>
2019-11-15 20:41:42 +1000

    update

--- Slim/runpipe.sh
+++ Slim/runpipe.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=TBpipe
+#SBATCH --output=TBpipe.%j.stdout
+#SBATCH --error=TBpipe.%j.stderr
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=16000 # mb
-#SBATCH --time=9999:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32000MB
 
 n=$SLURM_ARRAY_TASK_ID
-nme=$(head -n $n /home/larry/png_mtb/filesin.txt | tail -1)
+nme=$(head -n $n filesin.txt | tail -1 | cut -f 1 -d '_')
 
-docker run -it --rm -v /home/larry/png_mtb/:/data/ dockersubtest/qimr_mtb:slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
+docker run -it --rm -v ${pwd}/:/data/ dockersubtest/qimr_mtb:slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
