bd54487cbe5a449ca114d366516cf0ab277afd6e
Lachlan Coin <ljmcoin@gmail.com>
2019-11-22 10:21:38 +1000

    update

---
+++ Slim/cleanup.sh
@@ -0,0 +1,7 @@
+cd data/
+rm -f *.dict
+rm -rf temp_*
+rm -rf out_*
+cd ../
+rm -f *.stderr
+rm -f *.stdout
--- Slim/runpipe.sh
+++ Slim/runpipe.sh
@@ -6,9 +6,19 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32000MB
-
+echo $SLURM_ARRAY_TASK_ID
+if [ -z ${SLURM_ARRAY_TASK_ID+x} ]; then 
+	echo "SLURM_ARRAY_TASK_ID is unset, setting it to 1";
+        export SLURM_ARRAY_TASK_ID=1	
+#	exit 1; 
+else echo "SLURM_ARRAY_TASK_ID is set to '$SLURM_ARRAY_TASK_ID'"; 
+fi
 n=$SLURM_ARRAY_TASK_ID
 pwd=$(pwd)
 nme=$(ls ${pwd}/data | grep _R1.fastq.gz | sort | head -n $n  | tail -1 | cut -f 1 -d '_')
 echo "running "$nme
-docker run --rm -v ${pwd}/:/data/ dockersubtest/qimr_mtb:slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
+##USE FOLLOWING COMMAND TO LOAD DOCKER IMAGE WITHOUT RUNNING SCRIPT FOR DEBUGGING
+#docker run -it --entrypoint /bin/bash -v ${pwd}/data:/data/ dockersubtest/qimr_mtb:slim
+
+#FOLLOWING COMMAND RUNS THE SCRIPT
+docker run --rm -v ${pwd}/data:/data/ dockersubtest/qimr_mtb:slim /data/ $nme /data/H37Rv_refe /data/temp /data/out
