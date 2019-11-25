70976a18b77a2aedb67419285c79eb5a4b4ba7bd
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-11-25 15:13:28 +1000

    Update pipe2.sh

--- Slim/pipe2.sh
+++ Slim/pipe2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-#Example usage: ./pipe.sh /data/ RB17MT1804 /data/H37Rv_refe /data/temp /data/out
+#Example usage: ./pipe.sh /data/ /data/H37Rv_refe /data/temp /data/out
 
 #establishes variables
+DATE=$(date +"%Y%m%d")
+NUM=$(ls out/out_*/*_dup_alig.bam | wc -l )
+NAME=${NME}_${DATE}_${NUM} (edited) 
+input=$(ls out/out_*/*_dup_alig.bam)
+
 indir=$1
-shift
-NAME=$1
 shift
 reference=$1
 shift
@@ -16,23 +18,20 @@ shift
 
 
 
+gatk3 -T UnifiedGenotyper -R ${reference}.fasta -I ${input} -A AlleleBalance -pnrm EXACT_GENERAL_PLOIDY -ploidy 1 -glm SNP -o ${outdir}${NAME}.vcf
 
+gatk3 -T VariantFiltration -R ${reference}.fasta -V ${outdir}${NAME}.vcf --filterExpression "((DP-MQ0)<10) || ((MQ0/(1.0*DP))>=0.8) || (ABHom <0.8) || (Dels >0.5) || (QUAL > 90)" --filterName LowConfidence -o ${outdir}${NAME}_filtered.vcf
 
-#This command is to be run when all pairs have been processed, using data from all of them. WIP.
-
-bcftools view -h ${tempdir}${NAME} | grep "^#CHROM" | cut -f10-
-bcftools view -c1 -s ${NAME} -o ${tempdir}${NAME}_in.vcf ${tempdir}${NAME}.recode.vcf
+vcftools --vcf ${outdir}${NAME}_filtered.vcf --out ${tempdir}${NAME} --recode --keep-INFO-all
 
 python vcf_filter_module.py 9 ${tempdir}${NAME}_in.vcf ${tempdir}${NAME}_out.vcf
 
-#Commands below are to be run on the file generated in the previous step.
-#gatk3 -T CombineVariants -R /data/reference.fasta -–variant *.out.vcf -o master.vcf -genotypeMergeOptions UNIQUIFY
-## shortcut alternative for when testing with 1 file:
-#cat ${tempdir}${NAME}_out.vcf | bgzip -c > ${outdir}${NAME}_master.vcf.gz
+gatk3 -T CombineVariants -R ${reference}.fasta -–variant ${tempdir}${NAME}_out.vcf -o ${outdir}${NAME}_master.vcf -genotypeMergeOptions UNIQUIFY
 
-#zcat ${outdir}${NAME}_master.vcf.gz | vcf-to-tab > ${tempdir}${NAME}_snps.tab
 
-#/vcf_tab_to_fasta_alignment.pl -i ${tempdir}${NAME}_snps.tab > ${outdir}${NAME}_all_snps.fasta
+zcat ${outdir}${NAME}_master.vcf | vcf-to-tab > ${tempdir}${NAME}_snps.tab
+
+perl /vcf_tab_to_fasta_alignment.pl -i ${tempdir}${NAME}_snps.tab > ${outdir}${NAME}_all_snps.fasta
 
 #Make sure nectar users can access
 chmod -R 777 ${tempdir}
