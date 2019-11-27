f6520a7852fe805b0c7f98dc026a1c5c4f0fd81b
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-11-27 11:51:06 +1000

    Update vcf_filter_module.py

--- Slim/vcf_filter_module.py
+++ Slim/vcf_filter_module.py
@@ -10,7 +10,7 @@ import argparse, re, os
 
 def main():
     '''Do something'''
-    print "running the main routine"
+    print("running the main routine")
     
     '''Get command line arguments'''
     parser = argparse.ArgumentParser()
@@ -35,60 +35,60 @@ def main():
     second = ""
     initial = True
     for variant in sorted(variants, key=int):
-        print "working with " + variant
+        print("working with " + variant)
         if first == "":
             first = variant
-            print "added " + variant + " to first"
+            print("added " + variant + " to first")
         elif second == "":
             # ASSERT: ONLY GET HERE IF FIRST HAS GOOD OR NO LOWER BOUND
             if (int(variant) - int(first)) > int(args.bases):
                 if initial:
                     # INITIAL ENTRY AND GOOD UPPER BOUND
-                    print "writing initial " + first
+                    print("writing initial " + first)
                     with open(args.filtered_vcf, "a") as fpt:
                         fpt.write(variants[first] + "\n")
                     first = variant
-                    print "added  " + variant + " to first"
+                    print("added  " + variant + " to first")
                     initial = False
                 else:
                     # FIRST HAS GOOD LOWER AND UPPER BOUND
-                    print "writing first " + first
+                    print("writing first " + first)
                     with open(args.filtered_vcf, "a") as fpt:
                         fpt.write(variants[first] + "\n")
-                    print "added " + variant + " to first"
+                    print("added " + variant + " to first")
                     # NEW FIRST HAS GOOD LOWER BOUND
                     first = variant
             else:
                 # FIRST HAD GOOD LOWER BOUND BUT BAD UPPER
-                print "first and variant too close " + first + " " + variant
+                print("first and variant too close " + first + " " + variant)
                 # SECOND HAS BAD LOWER BOUND
                 second = variant
-                print "added  " + variant + " to second"
+                print("added  " + variant + " to second")
                 initial = False
         else:
             # ASSERT FIRST HAS BAD UPPER BOUND AND UNKNOWN LOWER
             # ASSERT SECOND HAS HAS BAD LOWER BOUND
             if (int(variant) - int(second)) > int(args.bases):
-                print "variant has good lower bound"
+                print("variant has good lower bound")
                 first = variant
-                print "added " + variant + " to first"
+                print("added " + variant + " to first")
                 second = ""
-                print "reset second"
+                print("reset second")
             else:
                 # FIRST HAS BAD UPPER AND UNKNOWN LOWER
                 # SECOND HAS BAD LOWER AND UPPER BOUND
                 # VARIANT HAS BAD LOWER BOUND
-                print "have second and variant has bad lower bound"
+                print("have second and variant has bad lower bound")
                 first = second
-                print "added second " + second + " to first"
+                print("added second " + second + " to first")
                 second = variant
-                print "added " + variant + " to second"
+                print("added " + variant + " to second")
     
     # ASSERT LAST CASE NEEDS WRITING IF GOOD LOWER BOUND
     # ASSERT SECOND IS SET IF HAS BAD LOWER BOUND 
     if second == "":
         # ASSERT: ONLY GET HERE IF FIRST HAS GOOD OR NO LOWER BOUND
-        print "writing first " + first
+        print("writing first " + first)
         with open(args.filtered_vcf, "a") as fpt:
             fpt.write(variants[first] + "\n")      
 
