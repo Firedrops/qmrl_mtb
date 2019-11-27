3c82176d8495f590be94e603f75a50b0fccbe87c
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-11-27 12:54:02 +1000

    Update pipe2.sh

--- Slim/pipe2.sh
+++ Slim/pipe2.sh
@@ -16,6 +16,8 @@ shift
 outdir=$1_${NAME}/
 shift
 
+
+
 ls /out/out_*/*_dup_alig.bam > /out/bams.list
 
 mkdir ${outdir}
@@ -24,11 +26,13 @@ gatk3 -T UnifiedGenotyper -R ${reference}.fasta -I /out/bams.list -A AlleleBalan
 
 gatk3 -T VariantFiltration -R ${reference}.fasta -V ${outdir}${NAME}.vcf --filterExpression "((DP-MQ0)<10) || ((MQ0/(1.0*DP))>=0.8) || (ABHom <0.8) || (Dels >0.5) || (QUAL > 90)" --filterName LowConfidence -o ${outdir}${NAME}_filtered.vcf
 
+#bcftools view -h ${outdir}${NAME}_filtered.vcf | grep "^#CHROM" | cut -f10-
+
 vcftools --vcf ${outdir}${NAME}_filtered.vcf --out ${tempdir}${NAME} --recode --keep-INFO-all
 
 python vcf_filter_module.py 9 ${tempdir}${NAME}_in.vcf ${tempdir}${NAME}_out.vcf
 
-gatk3 -T CombineVariants -R ${reference}.fasta -–variant ${tempdir}${NAME}_out.vcf -o ${outdir}${NAME}_master.vcf -genotypeMergeOptions UNIQUIFY
+#gatk3 -T CombineVariants -R ${reference}.fasta -–variant ${tempdir}${NAME}_out.vcf -o ${outdir}${NAME}_master.vcf -genotypeMergeOptions UNIQUIFY
 
 zcat ${outdir}${NAME}_master.vcf | vcf-to-tab > ${tempdir}${NAME}_snps.tab
 
