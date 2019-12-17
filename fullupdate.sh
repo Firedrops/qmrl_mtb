f59ef99c9f87da28be4e82772ce351f8a81a6ab4
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-12-17 22:08:08 +1000

    Update fullupdate.sh

--- fullupdate.sh
+++ fullupdate.sh
@@ -6,6 +6,6 @@ docker run -itd --entrypoint /bin/bash qimr_slim
 curr_docker=`docker container ls | grep qimr | awk '{print $1}'`
 docker commit $curr_docker dockersubtest/qimr_mtb:slim
 docker push dockersubtest/qimr_mtb:slim
-docker container docker stop $curr_docker
+docker stop $curr_docker
 unset curr_docker
 sbatch --array 1-5 clearslurm.sh
